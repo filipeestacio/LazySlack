054d7d6b3c21dc3a95ebb60d31f8e2d4b6ef5aa1
Filipe Estacio <filipe.estacio@aircall.io>
2026-03-25 17:06:34 +0000

    feat: add lip gloss style definitions

---
+++ internal/ui/styles/styles.go
@@ -0,0 +1,90 @@
+package styles
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	AppBorder = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62"))
+
+	SidebarStyle = lipgloss.NewStyle().
+			Width(30).
+			Padding(1, 1)
+
+	SidebarSelected = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("57")).
+			Bold(true).
+			Padding(0, 1)
+
+	SidebarNormal = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("252")).
+			Padding(0, 1)
+
+	SidebarSection = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241")).
+			Bold(true).
+			MarginTop(1).
+			Padding(0, 1)
+
+	MessagesStyle = lipgloss.NewStyle().
+			Padding(1, 1)
+
+	MessageUsername = lipgloss.NewStyle().
+			Bold(true)
+
+	MessageTimestamp = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("241"))
+
+	MessageText = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("252"))
+
+	MessageReaction = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("236")).
+			Padding(0, 1)
+
+	ThreadBorder = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("63")).
+			Padding(1, 1)
+
+	OverlayStyle = lipgloss.NewStyle().
+			Border(lipgloss.DoubleBorder()).
+			BorderForeground(lipgloss.Color("63")).
+			Padding(1, 2)
+
+	StatusBarStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("236")).
+			Padding(0, 1)
+
+	StatusBarKey = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("229")).
+			Background(lipgloss.Color("57")).
+			Bold(true).
+			Padding(0, 1)
+
+	InputStyle = lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("62")).
+			Padding(0, 1)
+
+	UnreadDot = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("212"))
+)
+
+var UsernameColors = []lipgloss.Color{
+	"204", "166", "178", "114", "81", "147", "212", "209",
+}
+
+func ColorForUser(userID string) lipgloss.Color {
+	var hash int
+	for _, c := range userID {
+		hash = hash*31 + int(c)
+	}
+	if hash < 0 {
+		hash = -hash
+	}
+	return UsernameColors[hash%len(UsernameColors)]
+}
